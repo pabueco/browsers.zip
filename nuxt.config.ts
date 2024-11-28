bdc01b52e3a73115314d15eb4f9af5cab26931a8
Pascal Bürkle <exxxodus.artz@gmail.com>
2024-11-28 23:20:23 +0100

    upgrade deps, replace lodash-es with es-toolkit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss", "@element-plus/nuxt", "@vueuse/nuxt"],
+
   elementPlus: {
     themes: ["dark"],
     importStyle: "scss",
   },
+
   ssr: false,
 
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
+          api: "modern-compiler",
           additionalData: `@use "@/assets/element.scss" as element;`,
         },
       },
@@ -23,8 +26,10 @@ export default defineNuxtConfig({
         {
           rel: "stylesheet",
           href: "https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;600;800&display=swap",
-        }
+        },
       ],
-    }
-  }
+    },
+  },
+
+  compatibilityDate: "2024-11-28",
 });
