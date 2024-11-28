9b6fe4f9cdfd4b25553227a9c0968c1d459997cd
Pascal Bürkle <exxxodus.artz@gmail.com>
2024-11-28 23:31:16 +0100

    introduce lifetime to $fetchWithCache

--- utils/index.ts
+++ utils/index.ts
@@ -3,20 +3,37 @@ import { Detector } from "detector-js";
 export const ucfirst = (str: string | undefined) =>
   str ? str.charAt(0).toUpperCase() + str.slice(1) : str;
 
-export const $fetchWithCache = async <T>(url: string) => {
+const DEFAULT_FETCH_CACHE_LIFETIME = 60 * 60 * 24; // 1 day
+
+export const $fetchWithCache = async <T>(
+  url: string,
+  expiresInSeconds: number = DEFAULT_FETCH_CACHE_LIFETIME
+) => {
   if (process.env.NODE_ENV !== "development") {
     return await $fetch<T>(url);
   }
 
   const cacheKey = `fetch:${url}`;
+  const expiresAtKey = `${cacheKey}:expiresAt`;
+
   const cached = localStorage.getItem(cacheKey);
 
-  if (cached) {
+  const cacheExpiresAt = localStorage.getItem(expiresAtKey);
+  const now = Date.now();
+  const hasExpired = !cacheExpiresAt || now > Number(cacheExpiresAt);
+
+  if (cached && !hasExpired) {
     return JSON.parse(cached) as T;
   }
 
   const result = await $fetch<T>(url);
+
   localStorage.setItem(cacheKey, JSON.stringify(result));
+  localStorage.setItem(
+    expiresAtKey,
+    (now + expiresInSeconds * 1000).toString()
+  );
+
   return result;
 };
 
